4c42380240bfdfd82882f8d082a410f5d0b079c1
yeon004 <kiminju071@gmail.com>
2026-04-06 01:02:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
 const CACHE_NAME = "looply-v1";
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    // 그냥 통과 (기본 네트워크 요청)
 });
